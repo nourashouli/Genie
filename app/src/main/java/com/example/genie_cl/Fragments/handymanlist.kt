10ab65376ced1e417694af41ae9a50a5526b16d9
noura <nourashouli@gmail.com>
2020-02-21 01:09:28 +0200

    anything

--- app/src/main/java/com/example/genie_cl/Fragments/handymanlist.kt
+++ app/src/main/java/com/example/genie_cl/Fragments/handymanlist.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.genie_cl.R
+import kotlinx.android.synthetic.main.fragment_handymanlist.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,9 +19,18 @@ class handymanlist : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+       // val text:String? = arguments!!.getString("category")
+//category.setText(text)
+// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_handymanlist, container, false)
     }
-
-
 }
+
+/*
+}
+ public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    Bundle savedInstanceState) {
+          String strtext=getArguments().getString("message");
+
+    return inflater.inflate(R.layout.fragment, container, false);
+ */
--- app/src/main/java/com/example/genie_cl/adapter/ServiceAdapter.kt
+++ app/src/main/java/com/example/genie_cl/adapter/ServiceAdapter.kt
@@ -1,22 +1,25 @@
 package com.example.genie_cl.adapter
 
+import android.content.ContentResolver
 import android.content.Context
-import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.genie_cl.R
-
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.genie_cl.Models.CardView
+import com.example.genie_cl.Fragments.handymanlist
+import com.example.genie_cl.MainActivity
 import com.example.genie_cl.Utils.Utils
+import kotlinx.android.synthetic.main.fragment_handymanlist.*
 import kotlinx.android.synthetic.main.service_card.view.*
 import org.json.JSONObject
 
-class ServiceAdapter(context: Context) : RecyclerView.Adapter<ServiceAdapter.ViewHolder>() {
-    var list: ArrayList<Any> = ArrayList()
 
+class ServiceAdapter(var context : Context) : RecyclerView.Adapter<ServiceAdapter.ViewHolder>() {
+    var list: ArrayList<Any> = ArrayList()
+   // var context:Context = context
     fun setItem(ob: Any) {
         list.add(ob)
         notifyItemInserted(list.size - 1)
@@ -48,21 +51,38 @@ class ServiceAdapter(context: Context) : RecyclerView.Adapter<ServiceAdapter.Vie
         Glide
             .with(holder.itemView)
             .load(Utils.BASE_IMAGE_URL.plus(image_url)).into(holder.itemView.service_image)
-        holder.itemView.setOnClickListener {
-//            val intent = Intent(  this, SignupActivity::class.java)
-//
-//            intent.flags =
-//                Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-//
-//            startActivity(intent)
+        holder.itemView.service_image.setOnClickListener{
+
+            //{itemClick(layoutPosition)}
+            (context as MainActivity).navigateToFragment(handymanlist())
+
+
+
+
+//            val bundle:Bundle = context.instance
+//            ba
+//            bundle.putString("category", (list[position] as JSONObject).getString("name"))
+//            //set Fragmentclass Arguments
+//            //set Fragmentclass Arguments
+//            val fragobj = handymanlist()
+//            fragobj.setArguments(bundle)
+
+
+
+
+
         }
     }
+
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+       // itemView.setOnClickListener( {itemClick(layoutPosition)} )
+    }
 
 }
 
