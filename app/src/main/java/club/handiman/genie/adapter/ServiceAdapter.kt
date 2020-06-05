3e64e027f3fdf9fb41430df38a1f2ca9bddef3f6
mohammad <itani0369-@hotmail.com>
2020-06-05 17:16:09 +0300

    .

--- app/src/main/java/club/handiman/genie/adapter/ServiceAdapter.kt
+++ app/src/main/java/club/handiman/genie/adapter/ServiceAdapter.kt
@@ -1,29 +1,38 @@
 package club.handiman.genie.adapter
+
 import club.handiman.genie.Fragments.handymanlist
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import club.handiman.genie.MainActivity
 import com.example.genie_cl.R
 import club.handiman.genie.Utils.Utils
+import club.handiman.genie.Utils.putExtraJson
+import club.handiman.genie.requestForm
 import com.example.genie_cl.adapter.utils.AdapterListener
 import kotlinx.android.synthetic.main.service_card.view.*
 import org.json.JSONObject
 
 
-class ServiceAdapter(var context : Context) : RecyclerView.Adapter<ServiceAdapter.ViewHolder>() {
+class ServiceAdapter(var context: Context) : RecyclerView.Adapter<ServiceAdapter.ViewHolder>() {
     var list: ArrayList<Any> = ArrayList()
     var listener: AdapterListener? = null
 
-    constructor(  context: Context , listener: AdapterListener) : this(context) {
+    constructor(context: Context, listener: AdapterListener) : this(context) {
         this.listener = listener
     }
 
 
-   // var context:Context = context
+    // var context:Context = context
     fun setItem(ob: Any) {
         list.add(ob)
         notifyItemInserted(list.size - 1)
@@ -49,15 +58,35 @@ class ServiceAdapter(var context : Context) : RecyclerView.Adapter<ServiceAdapte
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.overflow.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(context!!, holder.itemView.overflow)
+            popupMenu.menuInflater.inflate(R.menu.menu_album, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.urgent_request -> {
+                        val intent = Intent(context!!, requestForm::class.java)
+                        var ob = JSONObject()
+                        ob.put("id", (list[position] as JSONObject).optString("_id"))
+                        intent.putExtraJson("object", ob!!)
+                        ContextCompat.startActivity(context!!, intent!!, null)
+                        Toast.makeText(context!!, "You Clicked : " + item.title, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                true
+            })
+            popupMenu.show()
+
+        }
         holder.itemView.service_title.text = (list[position] as JSONObject).getString("name")
-        val image_url = (list[position] as JSONObject).optString("image","image.png")
+        val image_url = (list[position] as JSONObject).optString("image", "image.png")
 
         Glide
             .with(holder.itemView)
             .load(Utils.BASE_IMAGE_URL.plus(image_url)).into(holder.itemView.service_image)
-        holder.itemView.service_image.setOnClickListener{
+        holder.itemView.service_image.setOnClickListener {
 
-//            //{itemClick(layoutPosition)}
+            //            //{itemClick(layoutPosition)}
             (context as MainActivity).navigateToFragment(handymanlist((list[position])))
 //            this.listener!!.onAction(list[position])
         }
@@ -68,9 +97,9 @@ class ServiceAdapter(var context : Context) : RecyclerView.Adapter<ServiceAdapte
         return list.size
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-       // itemView.setOnClickListener( {itemClick(layoutPosition)} )
+        // itemView.setOnClickListener( {itemClick(layoutPosition)} )
     }
 
 }
